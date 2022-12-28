c1e7c99506dd9b5a777607f6cbd4879bc3e69b9b
Pedro Gomes <pgestudos2022@gmail.com>
2022-12-28 20:48:03 -0300

    Ultimos ajustes no layout

--- front-web/src/components/Navbar/index.tsx
+++ front-web/src/components/Navbar/index.tsx
@@ -21,7 +21,7 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon text-primary"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="carrostop-navbar">
